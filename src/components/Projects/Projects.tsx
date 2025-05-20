b56bc203eb7deaa21727d559a481a3e13758062a
Vagelis Kosmiadis <97461782+kosmiadis@users.noreply.github.com>
2025-05-20 20:57:46 +0300

    Update Projects.tsx

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -53,7 +53,7 @@ const PROJECTS: ProjectType[] = [
         title: 'Realtime Chat Application', 
         description: 'A modern, full-stack realtime chat application featuring user authentication, live messaging, and online/offline presence detection. Built with Socket.IO for bi-directional communication and Radix UI for a polished interface, the app supports theming (light/dark mode), responsive design, and smooth user feedback through Sonner toasts. TanStack Query handles data fetching and caching, ensuring a seamless and reactive user experience. Users can send messages, see others’ online status in real time, and enjoy a sleek, highly interactive interface.', 
         techStack: ['React.js', 'Node.js', 'Express.js', 'MongoDB', 'Typescript', 'Socket.io'],
-        featuredImg: '/src/assets/chat-app/chat_7.png', 
+        featuredImg: chatAppImg_7, 
         images: [
             chatAppImg_1,
             chatAppImg_2,
@@ -77,7 +77,7 @@ const PROJECTS: ProjectType[] = [
         title: 'Url Shortener', 
         description: 'A full-stack URL shortener application with real-time analytics and secure user interaction. Each generated URL includes detailed usage statistics visualized through interactive charts. Users can manage their links with visibility toggles, enabling or disabling access. The project emphasizes both frontend interactivity and backend security, incorporating features like JWT authentication and encrypted user data.', 
         techStack: ['React.js', 'Node.js', 'Express.js', 'MongoDB', 'Typescript'],
-        featuredImg: '/src/assets/url_shortener/url_2.png',
+        featuredImg: urlShortImg_2,
         images: [
             urlShortImg_1,
             urlShortImg_2,
@@ -106,7 +106,7 @@ const PROJECTS: ProjectType[] = [
         title: 'Furniture Restoration Landing Page', 
         description: 'A simple landing page built for a local furniture restoration business. Developed using EJS templating and Express.js, the site includes a functional contact form with real-time email validation and integrated email delivery via Nodemailer. This project emphasizes clean design for small business needs.', 
         techStack: ['Express.js', 'EJS', 'Node.js'],
-        featuredImg: '/src/assets/chalk59/chalk59_1.png', 
+        featuredImg: chalk59Img_1, 
         images: [
             chalk59Img_1,
             chalk59Img_2,
@@ -125,7 +125,7 @@ const PROJECTS: ProjectType[] = [
         title: 'MyLibrary', 
         description: 'A full-stack digital bookshelf where users can manage the books they own and track those they plan to buy in a dedicated wishlist section. The application features authentication, form validation, and persistent state using cookies. The frontend offers a responsive and animated UI built with NextUI and Framer Motion, while the backend ensures secure data handling and user authentication using JWT and encrypted credentials.', 
         techStack: ['React.js', 'Node.js', 'Express.js', 'MongoDB'],
-        featuredImg: '/src/assets/my_library/library_2.png',
+        featuredImg: myLibraryImg_2,
         images: [
             myLibraryImg_1,
             myLibraryImg_2,
@@ -146,7 +146,7 @@ const PROJECTS: ProjectType[] = [
         title: 'Simple Landing Page', 
         description: 'A pixel-perfect recreation of a pre-built WordPress theme, developed to sharpen my CSS and UI implementation skills. The design was replicated using React and TailwindCSS, with state handled via Zustand.', 
         techStack: ['React.js', 'TailwindCSS', 'Zustand'],
-        featuredImg: '/src/assets/renovation_site/renovation_1.png',
+        featuredImg: renovation_1,
         images: [
             renovation_1,
             renovation_2,
